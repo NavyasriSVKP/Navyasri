56686b2f524fb2417dfd4e085432ab308266a583
LPT-11 <LPT-11@192.168.1.44>
2024-10-28 14:29:29 +0530

    third commit

---
+++ SVKP/src/BasicsofJava/ArrayDemo.java
@@ -0,0 +1,12 @@
+package BasicsofJava;
+
+public class ArrayDemo {
+public static void main(String args[])
+
+{
+	int num[]= {3,5,7,9};
+	System.out.println(num[1]);
+	System.out.println(num[3]);
+	
+}
+}
