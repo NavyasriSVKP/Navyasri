867a7881d5354098a8906fc8525e9672c5c0d0db
LPT-11 <LPT-11@192.168.1.36>
2024-11-04 16:44:11 +0530

    fourth commit

---
+++ SVKP/src/BasicsofJava/DefaultValuesCheckingDemo.java
@@ -0,0 +1,15 @@
+package BasicsofJava;
+ class Demo
+ {
+	String strvalue1="navya";
+	int a[]= {1,2,3,4,5};
+ }
+
+public class DefaultValuesCheckingDemo {
+public static void main(String args[])
+{
+Demo d1=new Demo();
+ System.out.println(strvalue1);
+ System.out.println(d1.a);
+}
+}
