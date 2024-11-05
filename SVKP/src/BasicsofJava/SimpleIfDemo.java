ba231e1c0536bce85f1685156d44de34b382bbd6
LPT-11 <LPT-11@192.168.1.36>
2024-11-05 14:24:07 +0530

    Fifth commit

---
+++ SVKP/src/BasicsofJava/SimpleIfDemo.java
@@ -0,0 +1,12 @@
+package BasicsofJava;
+
+public class SimpleIfDemo {
+	public static void main(String args[])
+	{
+		int age=22;
+		if(age>18)
+		{
+System.out.println("You are Eligible");
+}
+}
+}
