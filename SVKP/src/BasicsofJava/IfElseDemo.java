5d15471690c8cc57bda9bcf2bbba68a324bfedd6
LPT-11 <LPT-11@192.168.1.36>
2024-11-05 14:29:18 +0530

    Fifth commit

---
+++ SVKP/src/BasicsofJava/IfElseDemo.java
@@ -0,0 +1,17 @@
+package BasicsofJava;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		int age=20;
+		if(age>22)
+		{
+			System.out.println("You are Eligible");
+		}
+		else
+		{
+			System.out.println("You are not Eligible");
+		}
+	}
+
+}
