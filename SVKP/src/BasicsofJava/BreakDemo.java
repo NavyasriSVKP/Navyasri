ce4f90aba1bf5b83193a300cd4324929aa09a026
LPT-11 <LPT-11@192.168.1.36>
2024-11-06 16:41:59 +0530

    Fifth commit

---
+++ SVKP/src/BasicsofJava/BreakDemo.java
@@ -0,0 +1,16 @@
+package BasicsofJava;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+for(int i=1;i<25;i++)
+{
+	if(i>15)
+	{
+		break;
+	}
+	System.out.println(i);
+}
+	}
+
+}
---
+++ SVKP/src/BasicsofJava/ForEachLoopDemo.java
@@ -0,0 +1,13 @@
+package BasicsofJava;
+
+public class ForEachLoopDemo {
+
+	public static void main(String[] args) {
+int num[]= {1,5,7,8,9,11,13};
+for(int numbers:num)
+{
+	System.out.println(numbers);
+}
+
+}
+}
