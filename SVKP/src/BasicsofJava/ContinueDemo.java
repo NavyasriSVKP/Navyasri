720e5bd3f8abc166ffcaef7276cd13e84d1754b5
LPT-11 <LPT-11@192.168.1.36>
2024-11-08 16:34:50 +0530

    Fifth Commit

---
+++ SVKP/src/BasicsofJava/ContinueDemo.java
@@ -0,0 +1,16 @@
+package BasicsofJava;
+
+public class ContinueDemo {
+
+	public static void main(String[] args) {
+for(int i=1;i<15;i++)
+{
+	if(i==8 || i==10)
+	{
+		continue;
+	}
+	System.out.println(i);
+   }
+ }
+
+}
