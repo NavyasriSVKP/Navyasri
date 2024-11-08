136fa31d82564710e2ff44174ea88273f293c8ae
LPT-11 <LPT-11@192.168.1.36>
2024-11-08 16:39:35 +0530

    Fifth Commit

---
+++ SVKP/src/BasicsofJava/ReturnDemo.java
@@ -0,0 +1,12 @@
+package BasicsofJava;
+
+public class ReturnDemo {
+
+	public static void main(String[] args) {
+    System.out.println(sum(8,3));
+	}
+	static int sum(int a,int b)
+	{
+return a+b;
+}
+}
