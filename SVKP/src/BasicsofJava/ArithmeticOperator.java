0687c3f67d7334297e1a4e6541d06b95a2939199
LPT-11 <LPT-11@192.168.1.36>
2024-11-13 14:22:41 +0530

    Sixth Commit

---
+++ SVKP/src/BasicsofJava/ArithmeticOperator.java
@@ -0,0 +1,23 @@
+package BasicsofJava;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+     int a=9,b=8;
+     //Addition
+     int sum=a+b;
+     System.out.println("Addition:" +sum);
+     //Subtraction
+     int diff=a-b;
+     System.out.println("Subtraction:"+diff);
+     //Multiplication
+     int Product=a*b;
+     System.out.println("Multiplication"+Product);
+     //Division
+     int Quotient=a/b;
+     System.out.println("Division"+Quotient);
+     //Modulus
+     int Remainder=a%b;
+     System.out.println("Remainder"+Remainder); 
+	}
+}
