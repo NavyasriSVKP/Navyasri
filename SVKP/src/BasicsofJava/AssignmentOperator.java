83131f554742c393605833640d611e5cc91f0cec
LPT-11 <LPT-11@10.0.0.112>
2024-11-15 16:46:36 +0530

    Sixth Commit

---
+++ SVKP/src/BasicsofJava/AssignmentOperator.java
@@ -0,0 +1,24 @@
+package BasicsofJava;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		int a=6;
+		//Add and assign
+		a+=8;
+		System.out.println("a after +=" +a);
+		//Sub and assign
+		a-=5;
+		System.out.println("a after -=" +a);
+		//Multiply and assign
+		a*=3;
+		System.out.println("a after *=" +a);
+		//Divide and assign
+		a/=2;
+		System.out.println("a after /=" +a);
+		//Modulus and assign
+		a%=4;
+		System.out.println("a after %=" +a);
+	}
+
+}
