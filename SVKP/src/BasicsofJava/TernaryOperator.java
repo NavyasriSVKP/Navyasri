c7d9ba7e9008decaabd39749a55697210ab0a3d7
LPT-11 <LPT-11@10.0.0.100>
2024-11-18 14:02:50 +0530

    Seventh Commit

---
+++ SVKP/src/BasicsofJava/TernaryOperator.java
@@ -0,0 +1,11 @@
+package BasicsofJava;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+    int a=4,b=9;
+    int Max=(a>b)?a:b;
+    System.out.println("Max value is: " + Max);
+	}
+
+}
