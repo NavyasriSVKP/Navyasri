b235fc5b35e9cba1b2325bb2bbbe2ecf83a67597
LPT-11 <LPT-11@10.0.0.100>
2024-11-18 13:57:43 +0530

    Seventh Commit

---
+++ SVKP/src/BasicsofJava/BitwiseOperator.java
@@ -0,0 +1,22 @@
+package BasicsofJava;
+
+public class BitwiseOperator {
+
+	public static void main(String[] args) {
+   int a=8;
+   int b=7;
+   //Bitwise AND Operator
+   System.out.println("(a&b): " + (a&b));
+   //Bitwise OR Operator
+   System.out.println("(a/b): " + (a/b));
+   //Bitwise XOR Operator
+   System.out.println("(a^b): " + (a^b));
+   //Bitwise NOT Operator
+   System.out.println("~a: " + (~a));
+   //Bitwise Left Shift
+   System.out.println("(a<<4) " + (a<<4));
+   //Bitwise Right shift
+   System.out.println("(a>>5) " + (a>>5));
+	}
+
+}
